c1924519687791379aba3102cb86160183ba32ad
Matthew Luu <mattmluu@gmail.com>
2022-05-05 00:44:36 -0700

    integrated front end and back end, some styling

--- backend/server/index.js
+++ backend/server/index.js
@@ -8,21 +8,23 @@ app.use(express.json());
 app.get('/test/server', (req, res) => {res.send({results: 'success'})})
 
 app.get('/word/secret', (req, res) => {
+  console.log('this route was hit')
   Word.findRandom().limit(1)
     .then((result) => {
       word = result[0].word
+      console.log(word)
       res.send(word)
     })
     .catch((error) => res.send(error))
 })
 
 app.post('/word/add', (req, res) => {
-  let word = req.query
-  Word.findOneAndUpdate(word, word, { new: true, upsert: true })
+  console.log(req.body);
+  Word.findOneAndUpdate(req.body, req.body, { new: true, upsert: true })
     .then((result) => res.status(200).send('nice'))
     .catch((error) => res.status(500).send(error))
 })
 
 app.listen(4000, () => {
-  console.log(`server listening on port:'${4000}`)
+  console.log(`server listening on port: ${4000}`)
 });
